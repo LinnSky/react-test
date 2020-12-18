436c495da058617040f7a590ee1611fe0b4a0483
Linn Fang <linn_fang@Linns-iMac.local>
2020-12-18 13:54:38 -0800

    Headline component tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from './components/header/index';
+import Headline from './components/headline/index';
 import './app.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <section class="main">
+        <Headline header="Posts" desc="Click the button to render posts"/>
+      </section>
     </div>
   );
 }
---
+++ src/components/headline/headline.spec.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Headline from './index';
+import { findByTestAtrr } from './../../../Utils';
+
+const setUp = (props) => {
+    const component = shallow(<Headline {...props}/>);
+    return component;
+};
+
+describe('Headline Component', () => {
+
+    describe('Have props', () => {
+        
+        let wrapper;
+        beforeEach(() => {
+            const props = {
+                header: 'Test Header',
+                desc: 'Test Desc'
+            };
+            wrapper = setUp(props);
+        });
+
+        it('Should render without errors', () => {
+            const component = findByTestAtrr(wrapper, 'HeadlineComponent');
+            expect(component.length).toBe(1);
+        });
+
+        it('Should render header', () => {
+            const component = findByTestAtrr(wrapper, 'header');
+            expect(component.length).toBe(1);
+        });
+
+        it('Should render description', () => {
+            const component = findByTestAtrr(wrapper, 'desc');
+            expect(component.length).toBe(1);
+        });
+        
+    });
+
+    describe('Have no props', () => {
+
+        let wrapper;
+        beforeEach(() => {
+            wrapper = setUp();
+        });
+
+        it('Should not render', () => {
+            const component = findByTestAtrr(wrapper, 'HeadlineComponent');
+            expect(component.length).toBe(0);
+        });
+    });
+    
+})
